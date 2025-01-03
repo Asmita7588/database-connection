4589fafe0f5b53c9565961180187e135dbb194fc
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2025-01-03 17:49:30 +0530

    Added: retrievs all record from employee payroll

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,7 +8,7 @@ import java.util.Enumeration;
 
 class Main {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/company"; //Database URL
+        String url = "jdbc:mysql://localhost:3306/payroll_service"; //Database URL
         String user = "root";
         String password = "root";
         try {
@@ -18,6 +18,11 @@ class Main {
             Statement stmt = connection.createStatement();// create a statement object to execute the queries. //create a table
             System.out.println("Connection Establish successfully");
 
+            String query = "select * from employee_payroll";
+           ResultSet rs = stmt.executeQuery(query);
+           while (rs.next()){
+               System.out.println("ID : " +rs.getInt("id") + " " + "Name : "+ rs.getString("name") + " " + "Salary = " + rs.getDouble("salary") );
+           }
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         } catch (SQLException e) {
